70c7714e257549fe6b407d779a8b69e49906c82b
soil-floating <89583593+soil-floating@users.noreply.github.com>
2024-08-26 01:25:33 +0800

    Add files via upload
    不知道啥时候写的汉诺塔，打印的是操作步骤。

---
+++ ws/game/hanoi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void hanoi(char A,char B,char C,int n){
+	if(n==1)
+		printf("%c -> %c\n",A,C);
+	else{
+		hanoi(A,C,B,n-1);
+		printf("%c -> %c\n",A,C);
+		hanoi(B,A,C,n-1);
+	}
+}
+int main(void){
+	char a = 'A';
+	char b = 'B';
+	char c = 'C';
+	int n;
+	printf("Enter your level of hanoi tower:");
+	scanf("%d",n);
+	printf("\n");
+	hanoi(a,b,c,n);
+}
