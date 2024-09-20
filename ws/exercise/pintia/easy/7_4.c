d1705c237e253b8d5aa962f437ceee23ac6a04d1
Mo_yu <89583593+soil-floating@users.noreply.github.com>
2024-09-21 00:22:17 +0800

    Create 7_4.c

---
+++ ws/exercise/pintia/easy/7_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    double a,b,c,d,e,f;
+    scanf("%lf,%lf",&a,&b);
+    c=((int)(a*100+0.5))/100.0;
+    d=((int)(b*100+0.5))/100.0;
+    e=a+b;
+    f=c+d;
+    printf("%-15lf+%15lf=%-15.2lf\n",a,b,e);
+    printf("%-15.2lf+%15.2lf=%-15.2lf\n",c,d,c+d);
+    printf("dev:%lf",(e-f)>0?(e-f):(f-e));
+    return 0;
+}
