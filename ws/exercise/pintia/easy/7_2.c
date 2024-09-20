d03ba63920cf3ec9c7e8970284434177d0dbaa44
Mo_yu <89583593+soil-floating@users.noreply.github.com>
2024-09-20 23:11:11 +0800

    Create 7_2.c
    一定注意浮点数运算精度

---
+++ ws/exercise/pintia/easy/7_2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int calculate(int sum,int amount,int* p);
+int main(){
+        float sum;
+        int sum_copy;
+        char* S[14] = {"NOTAS:","100.00","50.00","20.00","10.00","5.00","2.00","MOEDAS:","1.00","0.50","0.25","0.10","0.05","0.01"};
+        int s[14] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int divi[14] = {0,10000,5000,2000,1000,500,200,0,100,50,25,10,5,1};
+        scanf("%f",&sum);
+        sum_copy = sum*100;
+        for(int i=0;i<14;i++) (i==0 || i==7)?:(sum_copy = calculate(sum_copy,divi[i],&s[i]));
+        for(int i=0;i<14;i++) (i==0 || i==7)?printf("%s\n",S[i]):(i<7 ? printf("%d nota(s) de R$ %s\n",s[i],S[i]):printf("%d moeda(s) de R$ %s\n",s[i],S[i]));
+        return 0;
+}
+int calculate(int sum,int amount,int* p){
+        while(sum/amount>=1) sum -= amount,(*p)++;
+        return sum;
+}
