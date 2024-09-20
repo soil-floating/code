f81f4d39d5498655a625d2b24ab9b0eec1b1707b
Mo_yu <89583593+soil-floating@users.noreply.github.com>
2024-09-20 23:10:10 +0800

    Create 7_1.c

---
+++ ws/exercise/pintia/easy/7_1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+    int _sum,sum;
+    int num[7]={0,0,0,0,0,0,0};
+    char* amount[7]={"100,00","50,00","20,00","10,00","5,00","2,00","1,00",};
+    scanf("%d",&sum);
+    _sum = sum;
+    while(sum/100>=1){
+        num[0]++;
+        sum -= 100;
+    }
+    while(sum/50>=1){
+        num[1]++;
+        sum -= 50;
+    }
+    while(sum/20>=1){
+        num[2]++;
+        sum -= 20;
+    }
+    while(sum/10>=1){
+        num[3]++;
+        sum -= 10;
+    }
+    while(sum/5>=1){
+        num[4]++;
+        sum -= 5;
+    }
+    while(sum/2>=1){
+        num[5]++;
+        sum -= 2;
+    }
+    num[6] = sum;
+    printf("%d\n",_sum);
+    for(int i=0;i<7;i++) printf("%d nota(s) de R$ %s\n",num[i],amount[i]);
+    return 0;    
+}
