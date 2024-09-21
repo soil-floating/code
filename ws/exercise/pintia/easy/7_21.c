689575304fed1c2702bd4246a7b12f3208627181
Mo_yu <89583593+soil-floating@users.noreply.github.com>
2024-09-21 12:26:10 +0800

    Create 7_21.c

---
+++ ws/exercise/pintia/easy/7_21.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void print(int,char);
+int main(){
+    char ch = getchar();
+    for(int i=1;i<=3;i++){
+        print(i,ch);
+        putchar('\n');
+    }
+    for(int i=2;i>=1;i--){
+        print(i,ch);
+        i==1?:putchar('\n');
+    }
+    return 0;
+}
+void print(int i,char ch){
+    for(int k=1;k<=3-i;k++) putchar(' ');
+    for(int k=1;k<=i;k++) putchar(ch);
+    for(int k=1;k<=i-1;k++) putchar(ch);
+}
